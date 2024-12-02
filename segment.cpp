2eb42f8f29c34b48629f45d1acbdb5656d7877e8
neokoda <mneocicerok@gmail.com>
2024-12-02 19:58:11 +0700

    feat : checksum

--- segment.cpp
+++ segment.cpp
@@ -39,27 +39,40 @@ Segment finAck()
     return updateChecksum(segment);
 }
 
-uint8_t *calculateChecksum(Segment segment)
-{
-    // Simplified checksum calculation
-    uint8_t *checksum = new uint8_t[2]{0};
-    checksum[0] = (segment.seqNum & 0xFF);
-    checksum[1] = ((segment.seqNum >> 8) & 0xFF);
-    return checksum;
+uint16_t calculateChecksum(Segment segment) {
+    uint32_t sum = 0;
+
+    sum += segment.sourcePort;
+    sum += segment.destPort;
+    sum += (segment.seqNum >> 16) & 0xFFFF;
+    sum += segment.seqNum & 0xFFFF;
+    sum += (segment.ackNum >> 16) & 0xFFFF;
+    sum += segment.ackNum & 0xFFFF;
+    sum += (segment.data_offset << 4) | segment.reserved;
+    sum += (segment.flags.cwr << 15) | (segment.flags.ece << 14) | (segment.flags.urg << 13) |
+           (segment.flags.ack << 12) | (segment.flags.psh << 11) | (segment.flags.rst << 10) |
+           (segment.flags.syn << 9) | (segment.flags.fin << 8);
+    sum += segment.window;
+    sum += segment.urgentPointer;
+
+    for (size_t i = 0; i < 1460 / 2; i++) {
+        uint16_t data = (segment.payload[i * 2] << 8) | segment.payload[i * 2 + 1];
+        sum += data;
+    }
+
+    sum = (sum & 0xFFFF) + (sum >> 16);
+    return ~sum & 0xFFFF;
 }
 
 Segment updateChecksum(Segment segment)
 {
-    uint8_t *checksum = calculateChecksum(segment);
-    segment.checksum = (checksum[1] << 8) | checksum[0];
-    delete[] checksum;
+    uint16_t checksum = calculateChecksum(segment);
+    segment.checksum = checksum;
     return segment;
 }
 
 bool isValidChecksum(Segment segment)
 {
-    uint8_t *calculatedChecksum = calculateChecksum(segment);
-    uint16_t calculated = (calculatedChecksum[1] << 8) | calculatedChecksum[0];
-    delete[] calculatedChecksum;
-    return segment.checksum == calculated;
+    uint16_t calculatedChecksum = calculateChecksum(segment);
+    return segment.checksum == calculatedChecksum;
 }
